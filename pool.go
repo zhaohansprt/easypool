5660478a38b17f968d889022446998fcedd1bf51
han <botanyzh@live.cn>
2019-01-13 13:32:08 +0800

    fix

--- pool.go
+++ pool.go
@@ -8,54 +8,54 @@ import (
 )
 
 type HttpConnPool struct {
-	m          *sync.RWMutex
-	Max  ,
+	m *sync.RWMutex
+	Max,
 	IdleMax,
 	Inuse,
-	Free   int
-	insfunc   insFun
-	pool []*HttpConn
+	Free int
+	insfunc insFun
+	pool    []*HttpConn
 }
 
-type insFun func()interface{}
+type insFun func() interface{}
 type HttpConn struct {
-	Agent interface{}
+	Agent  interface{}
 	enable bool
 }
 
-func (p*HttpConnPool) String() string {
-	return fmt.Sprintf("free:%v, inuse:%v",p.Free, p.Inuse)
+func (p *HttpConnPool) String() string {
+	return fmt.Sprintf("free:%v, inuse:%v", p.Free, p.Inuse)
 }
-func (p*HttpConnPool) countreal() int {
+func (p *HttpConnPool) countreal() int {
 	p.m.RLock()
 	defer p.m.RUnlock()
 	return len(p.pool)
 
 }
-func (p*HttpConnPool) InitPool(l, i, max int,fun insFun){
-	p.m=new(sync.RWMutex)
+func (p *HttpConnPool) InitPool(l, i, max int, fun insFun) {
+	p.m = new(sync.RWMutex)
 	p.m.Lock()
 	defer p.m.Unlock()
 	v := reflect.ValueOf(fun())
-	if v.Kind() != reflect.Ptr{
+	if v.Kind() != reflect.Ptr {
 		panic("the InsFun only accept the pointer of the client instance!!")
 	}
-	p.insfunc=fun
+	p.insfunc = fun
 	//fmt.Println("pool init ok")
-	for ;l>0;l--{
+	for ; l > 0; l-- {
 		fmt.Println("inti pool loop")
-		p.Pool=append(p.pool, &HttpConn{p.insfunc(),true})
+		p.pool = append(p.pool, &HttpConn{p.insfunc(), true})
 	}
-	p.Free=l
-	p.Max=max
-	p.IdleMax=i
+	p.Free = l
+	p.Max = max
+	p.IdleMax = i
 	go func() {
-		for{
+		for {
 			p.m.Lock()
 
-			if p.Free >p.IdleMax{
-				for index,conn :=range p.pool {
-					if conn.enable{
+			if p.Free > p.IdleMax {
+				for index, conn := range p.pool {
+					if conn.enable {
 						p.remove(index)
 						break
 					}
@@ -70,49 +70,50 @@ func (p*HttpConnPool) InitPool(l, i, max int,fun insFun){
 	fmt.Println("pool init ok")
 }
 
-
-func (p*HttpConnPool) remove(i int)  {
-	p.pool=append(p.pool[:i], p.pool[i+1:]...)
+func (p *HttpConnPool) remove(i int) {
+	p.pool = append(p.pool[:i], p.pool[i+1:]...)
 	p.Free--
 }
 
-func (p*HttpConnPool) get() (conn *HttpConn,err error) {
+func (p *HttpConnPool) get() (conn *HttpConn, err error) {
 	p.m.Lock()
 	defer p.m.Unlock()
-	if p.Free>0{
-		for _,conn:=range p.pool{
-			if conn.enable{
-				{p.Inuse++; p.Free--}
-				conn.enable=false
+	if p.Free > 0 {
+		for _, conn := range p.pool {
+			if conn.enable {
+				{
+					p.Inuse++
+					p.Free--
+				}
+				conn.enable = false
 				return conn, nil
 			}
 		}
 	}
 
-	if p.Inuse<p.Max{
-		conn=&HttpConn{p.insfunc(),false}
-		p.pool=append(p.pool, conn)
+	if p.Inuse < p.Max {
+		conn = &HttpConn{p.insfunc(), false}
+		p.pool = append(p.pool, conn)
 		p.Inuse++
 		return
-	}else{
-		return nil,fmt.Errorf("pool is drain p.Free:%v  p.Inuse%v",p.Free, p.Inuse)
+	} else {
+		return nil, fmt.Errorf("pool is drain p.Free:%v  p.Inuse%v", p.Free, p.Inuse)
 	}
 
 }
 
-
-func (p*HttpConnPool) ret(conn *HttpConn) {
+func (p *HttpConnPool) ret(conn *HttpConn) {
 	p.m.Lock()
 	defer p.m.Unlock()
-	conn.enable=true
+	conn.enable = true
 	p.Free++
 	p.Inuse--
 
 }
-func (c*HttpConn) ret(P*HttpConnPool) {
+func (c *HttpConn) ret(P *HttpConnPool) {
 	P.m.Lock()
 	defer P.m.Unlock()
-	c.enable=true
+	c.enable = true
 	P.Free++
 	P.Inuse--
 
